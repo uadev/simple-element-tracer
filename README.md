8eda52e82108b584d3d53502105644efcc69a2da
Yuriy Voziy <yuretsz@gmail.com>
2018-02-24 03:30:42 +0200

    Async assign data from stream to a variable

--- README.md
+++ README.md
@@ -15,6 +15,7 @@ $ node index.js node index.js original.html diff1.html [diff2.html ...  diffN.ht
 [x] Make it easy to test with `npm test`
 [x] Add support for more than one diff page
 [x] Read file data from stream
+[x] Async assign stream data to string
 [] Read target element props
 [] Add help cli usage text
 [] Add README.md description about matching rules
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const {parse: parseUrl} = require('url');
 const sampleElementCSSPath = '#make-everything-ok-button';
 
 const path2Uri = function (path) {
-    let hasProtocol = parseUrl(path).protocol;
+    const hasProtocol = parseUrl(path).protocol;
     return hasProtocol ? path : fileUrl(path);
 }
 
@@ -14,24 +14,32 @@ function getFileList() {
   return process.argv.slice(2);
 }
 
-const files = getFileList().map(path2Uri);
-const originalFileUri = files.shift();
 
-console.log('Original file:', originalFileUri, 'diffs:', files);
-
-function readFromStream(rs) {
+async function readFromStream(rs) {
     let data = '';
     return new Promise(function(resolve, reject) {
-        // resolve with location of saved file
         rs.on('data', chunk => data += chunk);
         rs.on('end', () => resolve(data));
         rs.on("error", reject);
     })
 }
 
-getUri(originalFileUri)
-    .then(readFromStream)
-    .then(console.log)
-    .catch(console.error);
+function getOriginalElementSigns() {
 
-console.log(originalFileData);
+}
+
+function readDataFromUri(uri) {
+    return getUri(uri)
+        .then(readFromStream);
+}
+
+(async () => {
+    const files = getFileList().map(path2Uri);
+    const originalFileUri = files.shift();
+
+    console.log('Original file:', originalFileUri, "\ndiffs:", files);
+
+    const originalFileData = await readDataFromUri(originalFileUri);
+    console.log(originalFileData);
+})();
+
