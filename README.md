db8c80de6cedb1ab6cf68d891d8d95166c640b4e
Yuriy Voziy <yuretsz@gmail.com>
2018-02-24 02:05:10 +0200

    Read file data from generated streams

--- README.md
+++ README.md
@@ -14,6 +14,7 @@ $ node index.js node index.js original.html diff1.html [diff2.html ...  diffN.ht
 [x] Support both local and remote URIs as arguments
 [x] Make it easy to test with `npm test`
 [x] Add support for more than one diff page
+[x] Read file data from stream
 [] Read target element props
 [] Add help cli usage text
 [] Add README.md description about matching rules
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const fileUrl = require('file-url');
 const getUri = promisify(require('get-uri'));
 const {parse: parseUrl} = require('url');
 
+const sampleElementCSSPath = '#make-everything-ok-button';
+
 const path2Uri = function (path) {
     let hasProtocol = parseUrl(path).protocol;
     return hasProtocol ? path : fileUrl(path);
@@ -17,6 +19,19 @@ const originalFileUri = files.shift();
 
 console.log('Original file:', originalFileUri, 'diffs:', files);
 
-//getUri(originalFileUri)
-//    .then(rs => rs.pipe(process.stdout))
-//    .catch(console.error);
+function readFromStream(rs) {
+    let data = '';
+    return new Promise(function(resolve, reject) {
+        // resolve with location of saved file
+        rs.on('data', chunk => data += chunk);
+        rs.on('end', () => resolve(data));
+        rs.on("error", reject);
+    })
+}
+
+getUri(originalFileUri)
+    .then(readFromStream)
+    .then(console.log)
+    .catch(console.error);
+
+console.log(originalFileData);
